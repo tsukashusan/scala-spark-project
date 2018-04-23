5f8c02147124bc7e942006c099c5670088d808c0
Shuichi Tsukamoto <shtsukam@microsoft.com>
2018-04-23 21:07:28 +0900

    update for spark2.2.

--- build.sbt
+++ build.sbt
@@ -6,8 +6,8 @@ lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.11.11",
-      version      := "0.2.0-SNAPSHOT"
+      scalaVersion := "2.11.12",
+      version      := "0.1.0-SNAPSHOT"
     )),
     name := "Hello",
     resolvers ++= Seq("hortonworks release" at "http://repo.hortonworks.com/content/repositories/",
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  lazy val spakCore = "org.apache.spark" % "spark-core_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakSQL = "org.apache.spark" % "spark-sql_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakStreaming = "org.apache.spark" % "spark-streaming_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakMlib = "org.apache.spark" % "spark-mllib_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakCore = "org.apache.spark" % "spark-cloud_2.11" % "2.2.0.3.0.0.3-2" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakSQL = "org.apache.spark" % "spark-sql_2.11" % "2.2.0.3.0.0.3-2" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakStreaming = "org.apache.spark" % "spark-streaming_2.11" % "2.2.0.3.0.0.3-2" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakMlib = "org.apache.spark" % "spark-mllib_2.11" % "2.2.0.3.0.0.3-2" % "provided" exclude("org.apache.zookeeper", "zookeeper")
 }
